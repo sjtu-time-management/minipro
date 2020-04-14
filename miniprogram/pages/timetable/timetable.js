f58ebc808f9c4be3887bd7418e20fbeabf1baeab
initializer-z <954305012@qq.com>
2020-04-14 22:22:23 +0800

    delete

--- miniprogram/pages/timetable/timetable.js
+++ miniprogram/pages/timetable/timetable.js
@@ -26,11 +26,24 @@ Page({
     console.log(e);
     var index = e.currentTarget.dataset.index;
     var records=this.data.records;
+    db.collection('test2').doc(this.data.records[index]._id).remove({
+      success: res => {
+        wx.showToast({
+          title: '删除成功',
+        })
+      },
+      fail: err => {
+        wx.showToast({
+          icon: 'none',
+          title: '删除失败',
+        })
+        console.error('[数据库] [删除记录] 失败：', err)
+      }
+    })
     records.splice(index,1);
     this.setData({
       records:records
     });
-    wx.setStorageSync('records', this.data.records);
   },
   onShow: function () {
 
@@ -44,9 +57,6 @@ Page({
       disabled: this.data.disabled = false
     });
     var _this = this;  
-    const db = wx.cloud.database({   
-      env: 'engineering-dvsy5'
-    })  
     db.collection('test2').get({   
       success: res => {
         console.log(res.data)
