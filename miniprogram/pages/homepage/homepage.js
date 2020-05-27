d8176a466dd189bea7c21aad5e0b4f01cf4ef1cf
R2-D2isatrashcan <61978045+R2-D2isatrashcan@users.noreply.github.com>
2020-05-28 00:34:22 +0800

    自闭了

--- miniprogram/pages/homepage/homepage.js
+++ miniprogram/pages/homepage/homepage.js
@@ -1,7 +1,22 @@
 Page({
+  data: { 
+    spinstatus:true
+    },
   navg:function(){
     wx.navigateTo({
       url: '/pages/settings/settings',
     })
+  },
+  onShow:function(){
+    var that = this;
+    this.setData({
+      spinstatus: true
+    })
+    setTimeout(function () {
+      that.setData({
+        spinstatus: false
+      })
+
+    }, 2000)
   }
 })
