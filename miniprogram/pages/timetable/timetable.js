07aec3ff3ebaceb45229dbec9c92249a82ac2f15
super0619 <2762088343@qq.com>
2020-05-29 18:21:35 +0800

    注释

--- miniprogram/pages/timetable/timetable.js
+++ miniprogram/pages/timetable/timetable.js
@@ -297,7 +297,7 @@ Page({
     var flag = records[index].noti_flag;
     del(flag, records, index, this);
   },
-  // 优化，写成类/删除失败
+  // 优化，写成类
   deleteEvent1: function (e) {
     var index = e.currentTarget.dataset.index;
     var records = this.data.records1;
@@ -362,6 +362,7 @@ this.setData({
     //this.onLoad()
     
     //change424
+    // 以下确定Load日期
     var time = util.formatTime(new Date());
     // 再通过setData更改Page()里面的data，动态更新页面的数据
     that.setData({
